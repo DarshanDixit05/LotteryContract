4333b0000d34f3abe84149855926de44ebd43413
DarshanDixit05 <darshandixit0@gmail.com>
2023-06-11 23:42:27 +0530

    deploy test

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,26 +1,33 @@
 pragma solidity ^0.4.17;
 
-contract Inbox {
+contract Lottery {
     address manager;
     address[] public players;
 
-    constructor() public {
+    function Lottery() public {
         manager = msg.sender;
     }
 
     function enter() public payable {
-        require(msg.value > .01 ether, "");
+        require(msg.value > .01 ether);
         players.push(msg.sender);
     }
 
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, block.timestamp, players));
     }
-
-    function winner() public {
-        require(players.length>0, "not enough players");
+    modifier restricted {
+        require(msg.sender == manager);
+        _;
+    }
+    function winner() public restricted {
         uint indx = random() % players.length;
         players[indx].transfer(address(this).balance);
         players = new address[](0);
+    }
+
+    function getPlayers() public view returns(address[])
+    {
+        return players;
     }
 }
